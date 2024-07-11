11a5e54630c8a8bfdbd7c6b8fc84896860f5f3ec
Dio Akmal Prayoga <dioa.prayoga@gmail.com>
2024-07-11 15:21:52 +0800

    add feature extract data from excel hauling distance

--- distance/urls.py
+++ distance/urls.py
@@ -3,4 +3,7 @@ from django.urls import path
 from distance import views
 
 app_name = "distance"
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("import/", views.import_data, name="import"),
+]
