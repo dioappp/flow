98d8fa265c92749c18601a6b213dea79cb8f1068
Dio Akmal Prayoga <dioa.prayoga@gmail.com>
2024-07-16 10:49:32 +0800

    add ellipse to loaderID

--- stb_loader/models.py
+++ stb_loader/models.py
@@ -4,6 +4,7 @@ from django.db import models
 # Create your models here.
 class loaderID(models.Model):
     unit = models.CharField(max_length=20)
+    ellipse = models.CharField(max_length=20, null=True)
 
     def __str__(self):
         return self.unit
