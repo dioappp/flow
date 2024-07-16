ffdb4f7e8eaa90c03e221c1c990dd324f3ee071d
Dio Akmal Prayoga <dioa.prayoga@gmail.com>
2024-07-16 08:33:25 +0800

    add new app "distance"

---
+++ distance/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from stb_loader.models import loaderID
+
+# Create your models here.
+class distance(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True)
+
+    loader = models.ForeignKey(loaderID, on_delete=models.SET_NULL, null = True)
+    blok_loading = models.FloatField()
+    elevasi_loading = models.IntegerField()
+    lokasi_dumping = models.CharField(max_length=20)
+    elevasi_dumping = models.IntegerField()
+    horizontal_distance = models.FloatField()
+    lokasi = models.CharField(max_length=10)
+    vertical_distance = models.FloatField()
