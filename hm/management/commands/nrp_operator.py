effa069bd7a634e4288d8b1951ad52665ac6562c
Dio Akmal Prayoga <dioa.prayoga@gmail.com>
2024-07-17 08:58:02 +0800

    add management command input nrp-operator

--- hm/management/commands/nrp_operator.py
+++ hm/management/commands/nrp_operator.py
@@ -3,17 +3,21 @@ from hm.models import Operator
 import pandas as pd
 
 
-def add_arguments(self, parser):
-    parser.add_argument(
-        "--file",
-        "-f",
-        dest="file",
-        action="store",
-        type=str,
-        help="Input lokasi file csv",
-    )
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--file",
+            "-f",
+            dest="file",
+            action="store",
+            type=str,
+            help="Input lokasi file csv",
+        )
 
-
-def handle(self, *args, **options):
-    file_loc = options.get("file")
-    df = pd.read_csv(file_loc, sep=",")
+    def handle(self, *args, **options):
+        file_loc = options.get("file")
+        df = pd.read_csv(file_loc, sep=";")
+        for i, row in df.iterrows():
+            Operator.objects.update_or_create(
+                NRP=row["nrp"], defaults={"operator": row["operator"]}
+            )
