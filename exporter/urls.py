c2bc57dafa2e177a0bc108dd022c38161dda406f
Dio Akmal Prayoga <dioa.prayoga@gmail.com>
2024-07-18 11:56:46 +0800

    add form control to each card

--- exporter/urls.py
+++ exporter/urls.py
@@ -4,4 +4,6 @@ from exporter import views
 app_name = "exporter"
 urlpatterns = [
     path("", views.index, name="index"),
+    path("standby", views.standby, name="standby"),
+    path("production", views.production, name="production"),
 ]
--- exporter/views.py
+++ exporter/views.py
@@ -4,3 +4,11 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "exporter/index.html")
+
+
+def standby(request):
+    pass
+
+
+def production(request):
+    pass
