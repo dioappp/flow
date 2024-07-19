9e288264db032252fb90f2c7551e3ac79909fba5
Dio Akmal Prayoga <dioa.prayoga@gmail.com>
2024-07-19 11:16:44 +0800

    add new column to truckID models

--- ritase/models.py
+++ ritase/models.py
@@ -5,6 +5,8 @@ from stb_loader.models import loaderID
 # Create your models here.
 class truckID(models.Model):
     jigsaw = models.CharField(max_length=20)
+    ellipse = models.CharField(max_length=20, null=True)
+    code = models.CharField(max_length=10, null=True)
     OB_capacity = models.IntegerField(null=True)
 
     def __str__(self):
