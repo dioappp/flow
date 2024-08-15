8c2a5057901713ec4bbb099bc5a52a3d48de1c79
Dio Akmal Prayoga <dioa.prayoga@gmail.com>
2024-08-15 14:08:15 +0800

    remove report app (unused)

--- report/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from report import views
-
-urlpatterns = [
-    path("", views.index, name="report.index"),
-    path("show", views.showreport, name="report.show"),
-]
