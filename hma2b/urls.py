4dd9c81c74e6645ec7740a481d8704b803b73425
Dio Akmal Prayoga <dioa.prayoga@gmail.com>
2024-08-19 09:03:19 +0800

    add hma2b

--- hma2b/urls.py
+++ hma2b/urls.py
@@ -4,4 +4,7 @@ from hma2b import views
 
 
 app_name = "hma2b"
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("cek_operator/", views.operator, name="cek_operator"),
+]
