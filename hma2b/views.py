e6046d22abe483078cc3c1bf76b5877230d82e98
Dio Akmal Prayoga <dioa.prayoga@gmail.com>
2024-08-19 13:22:55 +0800

    display hm di page

--- hma2b/views.py
+++ hma2b/views.py
@@ -14,21 +14,18 @@ def index(request):
 def operator(request):
     date_pattern = request.POST.get("date")
     shift_pattern = request.POST.get("shift")
-    cn_pattern = request.POST.get("cn")
-    eqtype = request.POST.get("eqtype")
 
     ts, te = get_shift_time(date_pattern, shift_pattern)
     ts = ts - timedelta(minutes=30)
 
     data = (
         hmOperator.objects.filter(
-            # Q(equipment=hauler_jigsaw),
+            Q(equipment__startswith="X") | Q(equipment__startswith="G"),
             Q(login_time__gte=ts, login_time__lt=te),
             # | Q(logout_time__gt=ts, logout_time__lte=te),
         )
-        .values("id", "NRP__operator", "NRP", "hm_start", "hm_end")
-        .order_by("hm_start")
+        .values("equipment", "NRP__operator", "NRP", "hm_start", "hm_end")
+        .order_by("equipment", "hm_start")
     )
-
     response = {"data": list(data)}
     return JsonResponse(response)
